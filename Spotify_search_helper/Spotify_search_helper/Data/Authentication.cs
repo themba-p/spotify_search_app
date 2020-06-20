61e288ef4681c5623d21549de2d9aaeff4f91e0f
Themba Mndebele <Mndebele.themba@outlook.com>
2020-06-21 00:48:55 +0200

    -Fixed issue where app would crash if user had revoked access to the app on the spotify website, app now re-authenticates  -added initial light and dark theme support and ability to switch during runtime  -added initial ability to trigger the playback of a playlist from the app, selected playlist will load on the current active device. Throws error if no active device  -added an alphabetical filter for the playlist view, allows user user to jump to the first playlist that starts with the selected letter  -added intro that displays that user needs to login if they want to use the app  -added ability to play multiple playlist, incomplete as paging is still required to get all songs

--- Spotify_search_helper/Spotify_search_helper/Data/Authentication.cs
+++ Spotify_search_helper/Spotify_search_helper/Data/Authentication.cs
@@ -30,10 +30,30 @@ namespace Spotify_search_helper.Data
             if (SpotifyClient == null)
                 await Auth();
 
-            return SpotifyClient;
+            //check if spotify client is still valid or has access been revoked
+            try
+            {
+                await SpotifyClient.UserProfile.Current();
+                return SpotifyClient;
+                //success, return client
+            }
+            catch (Exception)
+            {
+                //error, try to re-authenticate
+                await Auth(true);
+                return SpotifyClient;
+            }
+
         }
 
-        private static async Task Auth()
+
+        //private static async void ReAuthenticate()
+        //{
+        //    //delete existing file
+        //    await StartAuthentication();
+        //}
+
+        private static async Task Auth(bool reauthenticate = false)
         {
             if (string.IsNullOrEmpty(clientId) || string.IsNullOrEmpty(clientSecret))
             {
@@ -53,14 +73,14 @@ namespace Spotify_search_helper.Data
                     throw new NullReferenceException();
             }
 
-            await Start();
+            await Start(reauthenticate);
         }
 
-        private static async Task Start()
+        private static async Task Start(bool reauthenticate = false)
         {
             try
             {
-                if (File.Exists(CredentialsPath))
+                if (!reauthenticate && File.Exists(CredentialsPath))
                 {
                     var json = await File.ReadAllTextAsync(CredentialsPath);
                     var token = JsonConvert.DeserializeObject<AuthorizationCodeTokenResponse>(json);
@@ -68,6 +88,7 @@ namespace Spotify_search_helper.Data
                     var authenticator = new AuthorizationCodeAuthenticator(clientId, clientSecret, token);
                     authenticator.TokenRefreshed += (sender, tokenx) => File.WriteAllText(CredentialsPath, JsonConvert.SerializeObject(tokenx));
 
+                    //might throw an error if user revoked access to their spotify account
                     var config = SpotifyClientConfig.CreateDefault()
                       .WithAuthenticator(authenticator);
 
@@ -76,9 +97,9 @@ namespace Spotify_search_helper.Data
                 else
                     await StartAuthentication();
             }
-            catch (Exception)
+            catch (Exception e)
             {
-
+                ViewModels.Helpers.DisplayDialog("Authentication error", e.Message);
             }
         }
 
@@ -86,7 +107,9 @@ namespace Spotify_search_helper.Data
         {
             var request = new LoginRequest(_server.BaseUri, clientId, LoginRequest.ResponseType.Code)
             {
-                Scope = new List<string> { UserReadEmail, UserReadPrivate, PlaylistReadPrivate }
+                Scope = new List<string> { UserReadPrivate, PlaylistReadPrivate, UserModifyPlaybackState,
+                    UserLibraryModify, UserLibraryRead, PlaylistModifyPrivate, 
+                    PlaylistModifyPublic }
             };
 
             Uri uri = request.ToUri();
@@ -126,6 +149,13 @@ namespace Spotify_search_helper.Data
             else if (WebAuthenticationResult.ResponseStatus == WebAuthenticationStatus.ErrorHttp)
             {
                 ViewModels.Helpers.DisplayDialog("Authentication error!", "Error code: " + WebAuthenticationResult.ResponseErrorDetail + ", please try again.");
+            }
+            else if(WebAuthenticationResult.ResponseStatus == WebAuthenticationStatus.UserCancel)
+            {
+                SpotifyClient = null;
+                DataSource.AuthFailed();
+
+                //show that user can't use app if they are not logged in
             }
             else
             {
